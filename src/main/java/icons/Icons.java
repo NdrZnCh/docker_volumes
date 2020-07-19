ac2167baee22a98fdb921a0732964412f42e262d
Andrey Zinchenko <ndr.znch@gmail.com>
2020-07-19 20:56:47 +0200

    add icons

---
+++ src/main/java/icons/Icons.java
@@ -0,0 +1,9 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class Icons {
+  public static final Icon DOCKER_ICON = IconLoader.getIcon("/icons/docker.svg");
+}
