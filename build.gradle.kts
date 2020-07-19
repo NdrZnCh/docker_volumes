37e24b7eb150d0e1ddbe3f21d91f6ae323f289c1
Andrey Zinchenko <ndr.znch@gmail.com>
2020-07-19 20:56:32 +0200

    configure gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,10 +103,4 @@ tasks {
             changelog.getLatest().toHTML()
         })
     }
-
-    publishPlugin {
-        dependsOn("patchChangelog")
-        token(System.getenv("PUBLISH_TOKEN"))
-        channels(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
-    }
 }
