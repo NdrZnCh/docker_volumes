987ecf4d8c41acc3a61aea30473eb9cdb7d068ce
Andrey Zinchenko <ndr.znch@gmail.com>
2020-07-29 19:23:36 +0200

    WIP : improve volume create dialog

--- src/main/kotlin/docker/volumes/toolWindow/CreateVolumeDialogWrapper.kt
+++ src/main/kotlin/docker/volumes/toolWindow/CreateVolumeDialogWrapper.kt
@@ -1,18 +1,11 @@
 package docker.volumes.toolWindow
 
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.ui.LabeledComponent
-import com.intellij.ui.TableUtil
-import com.intellij.ui.ToolbarDecorator
 import com.intellij.ui.layout.LCFlags
 import com.intellij.ui.layout.panel
-import com.intellij.ui.table.JBTable
 import docker.volumes.DockerVolumesBundle
 import org.jetbrains.concurrency.runAsync
 import javax.swing.JComponent
-import javax.swing.JPanel
-import javax.swing.table.AbstractTableModel
-import javax.swing.table.DefaultTableCellRenderer
 
 
 class CreateVolumeDialogWrapper : DialogWrapper(true) {
@@ -20,7 +13,7 @@ class CreateVolumeDialogWrapper : DialogWrapper(true) {
     private var myVolumeDriver = "local"
     private var myVolumeIsReadonly: Boolean = false
 
-    private val myVolumeOptions: MutableList<Pair<String, String>> = mutableListOf()
+    private val optionsPanel = DockerVolumeOptionsPanel()
 
     var applyAction: (String) -> Unit = {}
 
@@ -30,20 +23,6 @@ class CreateVolumeDialogWrapper : DialogWrapper(true) {
     }
 
     override fun createCenterPanel(): JComponent? {
-        val tableModel = OptionsTableModel(myVolumeOptions)
-
-        val optionsTable = JBTable(tableModel).apply {
-            setDefaultRenderer(String::class.java, DefaultTableCellRenderer())
-        }
-
-        val optionsPanel = ToolbarDecorator.createDecorator(optionsTable).apply {
-            setAddAction {
-                myVolumeOptions.add(Pair("", ""))
-                tableModel.fireTableRowsInserted(myVolumeOptions.size - 1, myVolumeOptions.size - 1)
-            }
-            setRemoveAction { TableUtil.removeSelectedItems(optionsTable) }
-        }.createPanel().toLabeledComponent("Options:")
-
         return panel(LCFlags.fillX) {
             row("Name:") {
                 textField({ myVolumeName }, { myVolumeName = it }).withValidationOnInput {
@@ -51,9 +30,9 @@ class CreateVolumeDialogWrapper : DialogWrapper(true) {
 
                     when {
                         value.isBlank() -> null
-                        value.length < 2 -> error("Name length can't be lower than 2.")
-                        !"([a-zA-Z0-9])([a-zA-Z0-9_.-])+".toRegex().matches(value) -> {
-                            error("Only \"[a-zA-Z0-9][a-zA-Z0-9_.-]\" are allowed.")
+                        value.length < 2 -> error("Only empty or with length greater than 1 names are allowed.")
+                        !"([a-zA-Z0-9_.-])+".toRegex().matches(value) -> {
+                            error("Only '[a-zA-Z0-9_.-]' are allowed.")
                         }
                         else -> null
                     }
@@ -74,28 +53,4 @@ class CreateVolumeDialogWrapper : DialogWrapper(true) {
 
         runAsync { applyAction(myVolumeName) }
     }
-
-    private class OptionsTableModel(var options: MutableList<Pair<String, String>>) : AbstractTableModel() {
-        override fun getRowCount(): Int = options.size
-
-        override fun getColumnCount(): Int = 2
-
-        override fun getValueAt(rowIndex: Int, columnIndex: Int): Any {
-            val option = options[rowIndex]
-
-            return if (columnIndex == 0) option.first else option.second
-        }
-
-        override fun getColumnName(column: Int): String? {
-            return if (column == 0) "Name" else "Value"
-        }
-
-        override fun isCellEditable(rowIndex: Int, columnIndex: Int): Boolean {
-            return true
-        }
-    }
-}
-
-private fun JPanel.toLabeledComponent(title: String): JComponent {
-    return LabeledComponent.create(this, title)
 }
---
+++ src/main/kotlin/docker/volumes/toolWindow/DockerVolumeOptionsPanel.kt
@@ -0,0 +1,75 @@
+package docker.volumes.toolWindow
+
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.ui.AddEditRemovePanel
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.layout.ValidationInfoBuilder
+import com.intellij.ui.layout.panel
+import javax.swing.JComponent
+
+class DockerVolumeOptionsPanel : AddEditRemovePanel<Pair<String, String>>(
+        MyOptionsTableModel(), mutableListOf(), "Options:"
+) {
+    override fun removeItem(option: Pair<String, String>): Boolean {
+        return true
+    }
+
+    override fun editItem(option: Pair<String, String>): Pair<String, String>? {
+        return doAddOrEdit(option)
+    }
+
+    override fun addItem(): Pair<String, String>? {
+        return doAddOrEdit(null)
+    }
+
+    private fun doAddOrEdit(option: Pair<String, String>?): Pair<String, String>? {
+        val optionDialog = MyOptionsDialog(option)
+        return if (!optionDialog.showAndGet()) null else optionDialog.getValue().takeIf {
+            it.first.isNotBlank() && it.second.isNotBlank()
+        }
+    }
+
+    private class MyOptionsTableModel : TableModel<Pair<String, String>>() {
+        override fun getColumnCount(): Int = 2
+
+        override fun getColumnName(column: Int): String = if (column == 0) "Name" else "Value"
+
+        override fun getField(o: Pair<String, String>, c: Int): String = if (c == 0) o.first else o.second
+    }
+
+    private class MyOptionsDialog(option: Pair<String, String>?) : DialogWrapper(false) {
+        private var myOptionName: String = option?.first.orEmpty()
+        private var myOptionValue: String = option?.second.orEmpty()
+
+        private val validator: (ValidationInfoBuilder.(JBTextField) -> ValidationInfo?) = {
+            val value = it.text
+
+            when {
+                value.isBlank() -> error("Can't be empty")
+                !"([a-zA-Z0-9_.,{}=-])+".toRegex().matches(value) -> {
+                    error("Only '[a-zA-Z0-9_.,{}=-]' are allowed.")
+                }
+                else -> null
+            }
+        }
+
+        init {
+            init()
+            this.title = if (option == null) "Add new volume option" else "Edit volume options"
+        }
+
+        override fun createCenterPanel(): JComponent? {
+            return panel {
+                row("Name:") {
+                    textField({ myOptionName }, { myOptionName = it }).withValidationOnApply(validator).focused()
+                }
+                row("Value:") {
+                    textField({ myOptionValue }, { myOptionValue = it }).withValidationOnApply(validator)
+                }
+            }
+        }
+
+        fun getValue() = Pair(myOptionName, myOptionValue)
+    }
+}
