da8e6b8f87a3b6dbab1bac50f05556ef2a68a7cd
Andrey Zinchenko <ndr.znch@gmail.com>
2020-08-01 16:46:04 +0200

    fun -> inline fun

--- src/main/kotlin/utils/Utils.kt
+++ src/main/kotlin/utils/Utils.kt
@@ -7,15 +7,15 @@ import docker.communicator.Success
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-fun <K, V> Map<K, V>.filterIf(doFilter: Boolean, predicate: (Map.Entry<K, V>) -> Boolean): Map<K, V> {
+inline fun <K, V> Map<K, V>.filterIf(doFilter: Boolean, predicate: (Map.Entry<K, V>) -> Boolean): Map<K, V> {
     return if (doFilter) this.filter(predicate) else this
 }
 
-fun <V> Array<V>?.firstOr(or: () -> V) = this?.firstOrNull() ?: or()
+inline fun <V> Array<V>?.firstOr(or: () -> V) = this?.firstOrNull() ?: or()
+
+inline fun <V> Collection<V>.firstOr(or: () -> V): V = if (this.isEmpty()) or() else this.first()
 
 fun <V> Set<V>.minusIfNotNull(element: V?) = if (element != null) this.minus(element) else this
-
-fun <V> Collection<V>.firstOr(or: () -> V): V = if (this.isEmpty()) or() else this.first()
 
 fun runBackgroundProcess(processBuilder: ProcessBuilder, timeout: Long): Result<Process> {
     val process = try {
